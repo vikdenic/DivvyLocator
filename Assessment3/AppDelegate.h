f777b47ab88e6a1c4565ad6920599b1f0a1b4e46
vikdenic <vik.denic@gmail.com>
2014-05-30 16:08:19 -0500

    Initial Commit

---
+++ Assessment3/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Assessment3
+//
+//  Created by Vik Denic on 5/30/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Assessment3/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Assessment3
+//
+//  Created by Vik Denic on 5/30/14.
+//  Copyright (c) 2014 Mobile Makers. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
